af33bd3732e71bbb38df303983d12e0c1835fca5
kristjannomm <70632191+kristjannomm@users.noreply.github.com>
2021-11-30 09:49:14 +0200

    Update darknet.sh

--- darknet.sh
+++ darknet.sh
@@ -11,20 +11,20 @@ sudo sh cuda_11.5.1_495.29.05_linux.run
 echo
 echo Installing essential packages
 echo
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install build-essential cmake unzip pkg-config
-sudo apt-get install gcc-6 g++-6 
-sudo apt-get install libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev
-sudo apt-get install libjpeg-dev libpng-dev libtiff-dev
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-sudo apt-get install libxvidcore-dev libx264-dev
-sudo apt-get install libopenblas-dev libatlas-base-dev liblapack-dev gfortran
-sudo apt-get install libhdf5-serial-dev
-sudo apt-get install python3-dev python3-tk python-imaging-tk
-sudo apt-get install libgtk-3-dev
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install build-essential cmake unzip pkg-config - -y
+sudo apt-get install gcc-6 g++-6 -y
+sudo apt-get install libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev -y
+sudo apt-get install libjpeg-dev libpng-dev libtiff-dev -y
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y
+sudo apt-get install libxvidcore-dev libx264-dev -y
+sudo apt-get install libopenblas-dev libatlas-base-dev liblapack-dev gfortran -y
+sudo apt-get install libhdf5-serial-dev -y
+sudo apt-get install python3-dev python3-tk python-imaging-tk -y
+sudo apt-get install libgtk-3-dev -y
 sudo add-apt-repository ppa:graphics-drivers/ppa
-sudo apt update
+sudo apt update -y
 echo
 echo Installing CUDA toolkit
 echo
